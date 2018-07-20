8b784b5c051afaa353ba50b41da91559783b699e
Marianna Katsika <marianna.katsika@gmail.com>
2018-07-20 17:29:49 +0300

    profiles

---
+++ NOTES.md
@@ -0,0 +1,2 @@
+TODO: profile validation to date cannot be before from date - ok
+TODO: edit profile, leave stuff blank to delete - ok
---
+++ validation/experience.js
@@ -0,0 +1,33 @@
+const Validator = require("validator");
+const isEmpty = require("./is-empty");
+
+module.exports = function validateExperienceInput(data) {
+  let errors = {};
+
+  data.title = !isEmpty(data.title) ? data.title : "";
+  data.company = !isEmpty(data.company) ? data.company : "";
+  data.from = !isEmpty(data.from) ? data.from : "";
+  data.to = !isEmpty(data.to) ? data.to : "";
+
+  if (Validator.isEmpty(data.title)) {
+    errors.title = "Job title field is required";
+  }
+
+  if (Validator.isEmpty(data.company)) {
+    errors.company = "Company field is required";
+  }
+
+  if (Validator.isEmpty(data.from)) {
+    errors.from = "Start date field is required";
+  }
+
+  if (Validator.isAfter(data.from, data.to)) {
+    errors.from = "Start date must be before end date";
+    errors.to = "Start date must be before end date";
+  }
+
+  return {
+    errors,
+    isValid: isEmpty(errors)
+  };
+};
